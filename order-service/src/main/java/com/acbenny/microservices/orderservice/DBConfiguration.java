4428a985adeb6e65c78f0355652250ef614f9bc5
Arun Benny <acbenny@gmail.com>
2021-02-27 20:57:29 +0000

    Distributed database and deployment updated

--- order-service/src/main/java/com/acbenny/microservices/orderservice/DBConfiguration.java
+++ order-service/src/main/java/com/acbenny/microservices/orderservice/DBConfiguration.java
@@ -6,6 +6,8 @@ import com.orientechnologies.orient.core.db.ODatabaseSession;
 import com.orientechnologies.orient.core.db.OrientDB;
 import com.orientechnologies.orient.core.db.OrientDBConfig;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -15,6 +17,8 @@ import org.springframework.web.context.annotation.RequestScope;
 
 @Configuration
 public class DBConfiguration {
+    Logger log = LoggerFactory.getLogger(DBConfiguration.class);
+
     @Autowired
     private DiscoveryClient discoveryClient;
 
@@ -27,8 +31,16 @@ public class DBConfiguration {
     @Value("${db.password:admin}")
     private String password;
 
+    @Value("${HOSTNAME}")
+    private String hostname;
+
     private URI serviceUrl() {
-        return discoveryClient.getInstances("orientdb-svc-microservices")
+        log.info("Hostname:"+hostname);
+        int num = Integer.parseInt(hostname.replaceAll(".*[^[0-9]+]", ""));
+        String svcName = "orientdb-svc-microservices";
+        if (num%2!=0)
+            svcName = "orientdb-svc-secondary-microservices";
+        return discoveryClient.getInstances(svcName)
           .stream()
           .findFirst() 
           .map(si -> si.getUri())
@@ -41,6 +53,7 @@ public class DBConfiguration {
     @RequestScope
     public ODatabaseSession orientDBSessionFactory() {
         String path = serviceUrl().getHost();
+        log.info("Path:"+path);
         orient = new OrientDB("remote:" + path, OrientDBConfig.defaultConfig());
         return orient.open(databaseName, user, password);
     }
